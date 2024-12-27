30ad7e66e9efa4dd42862f4220d7a61d83919b2b
vasanthakumar <113460093+vasanthakumar-jpg@users.noreply.github.com>
2024-12-27 14:32:44 +0530

    Component Added

---
+++ cco/src/components/OtherTreatment.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+
+export default function OtherTreatment() {
+  return <>
+  <div>
+    
+    <div className='p-5'>
+    <ul>
+        <li className='text-mintGreen text-3xl font-bold'>
+            <a href="#Orthodontic Issues">Common Orthodontic Issues</a>
+        </li>
+    </ul>
+     <p className='pt-4 lg:text-2xl' id='Orthodontic Issues'>Orthodontic issues are more than just cosmetic concerns — they can also impact your oral health and overall well-being. At Mint Orthodontics, we specialize in diagnosing and treating a range of orthodontic conditions to help you achieve a healthier, straighter smile.</p>
+    <img src="https://mintortho.com/wp-content/uploads/2024/03/Accelerated-treatment-1024x681.jpg"
+    className='w-[550px] h-[400px] xl:w-full xl:h-full xl:px-5 py-5'
+    alt="" />
+
+    </div>
+    
+    <div className='p-5'>
+    <ul>
+        <li className='text-mintGreen text-3xl font-bold'>
+            <a href="#Accelerated treatment">Accelerated treatment</a>
+        </li>
+    </ul>
+     <p className='pt-4 lg:text-2xl' id='Accelerated treatment'>Time is not a surplus most of us have. We like things done well but we also like things done fast. Enter accelerated orthodontics with AcceleDent. Whether you get braces or Invisalign clear aligners, AcceleDent enables us to potentially cut your treatment time in half.</p>
+    <img src="https://mintortho.com/wp-content/uploads/2024/03/Night-Time-Only-Treatment-1024x681.jpg"
+    className='w-[550px] h-[400px] xl:w-full xl:h-full xl:px-5 py-5'
+    alt="" />
+
+    </div>
+
+    
+    <div className='p-5'>
+    <ul>
+        <li className='text-mintGreen text-3xl font-bold'>
+            <a href="#Night-Time Only Treatment">Night-Time Only Treatment</a>
+        </li>
+    </ul>
+     <p className='pt-4 lg:text-2xl' id='Night-Time Only Treatment'>Night-time treatment is an innovative approach to orthodontics that involves wearing your aligners only at night while you sleep. This is a great option for those who may not want to wear aligners during the day due to aesthetic concerns or lifestyle preferences.</p>
+    <img src="https://mintortho.com/wp-content/uploads/2024/03/Two-Phase-Treatment-1024x683.jpg"
+    className='w-[550px] h-[400px] xl:w-full xl:h-full xl:px-5 py-5'
+    alt="" />
+    </div>
+
+    <div className='p-5'>
+    <ul>
+        <li className='text-mintGreen text-3xl font-bold'>
+            <a href="#Two-Phase Treatment">Two-Phase Treatment</a>
+        </li>
+    </ul>
+     <p className='pt-4 lg:text-2xl' id='Two-Phase Treatment'>At Mint Orthodontics, we’re dedicated to providing comprehensive orthodontic care to patients of all ages, especially children with developing smiles. One of the specialized approaches we use for our younger patients is called the two-phase treatment.
+     Two-phase treatment is an orthodontic method specifically designed to maximize the opportunity to achieve healthy, functional, and aesthetically pleasing results that will remain stable throughout your child’s life.
+     </p>
+    <img src="https://mintortho.com/wp-content/uploads/2024/03/TMJ-Treatment-1024x683.jpg"
+    className='w-[550px] h-[400px] xl:w-full xl:h-full xl:px-5 py-5'
+    alt="" />
+    </div>
+
+    <div className='p-5'>
+    <ul>
+        <li className='text-mintGreen text-3xl font-bold'>
+            <a href="#TMJ Treatment">TMJ Treatment</a>
+        </li>
+    </ul>
+     <p className='pt-4 lg:text-2xl' id='TMJ Treatment'>At Mint Orthodontics, we understand that oral health extends beyond just beautiful smiles. 
+        That’s why we offer specialized treatment for Temporomandibular Joint Disorders (TMD), a group of conditions affecting the temporomandibular joint (TMJ) — the hinge connecting your jaw to your skull. Among the most common, irritating, and difficult-to-diagnose problems with head and neck pain are temporal mandibular joint problems, more commonly known as TMJ problems.
+     </p>
+    <img src="https://mintortho.com/wp-content/uploads/2024/03/Surgical-Orthodontics-1024x681.jpg"
+    className='w-[550px] h-[400px] xl:w-full xl:h-full xl:px-5 py-5'
+    alt="" />
+    </div>
+
+    <div className='p-5'>
+    <ul>
+        <li className='text-mintGreen text-3xl font-bold'>
+            <a href="#Surgical Orthodontics">Surgical Orthodonticst</a>
+        </li>
+    </ul>
+     <p className='pt-4 lg:text-2xl' id='Surgical Orthodontics'>Does “orthodontic surgery” give you horrible visions of drinking a cheeseburger through a straw? 
+        Now that we’ve implanted that thought into your head, try to brush it aside and make room for modern-day orthodontic surgery. While not necessary for most people, today’s surgical orthodontics is carefully planned and is a collaborative approach from your orthodontist and an oral surgeon.
+     </p>
+    <img src="https://mintortho.com/wp-content/uploads/2024/03/Retainers-1024x681.jpg"
+    className='w-[550px] h-[400px] xl:w-full xl:h-full xl:px-5 p-5'
+    alt="" />
+    </div>
+
+    <div className='p-5'>
+    <ul>
+        <li className='text-mintGreen text-3xl font-bold'>
+            <a href="#Retainers">Retainers</a>
+        </li>
+    </ul>
+     <p className='pt-4 lg:text-2xl' id='Retainers'>At Mint Orthodontics, we’re committed to ensuring that your journey toward a beautiful, healthy smile doesn’t end when the braces come off or you finish your last set of aligners. That’s where retainers come in, and to make this stage easier, 
+        we offer Retain to ensure your smile stays straight, forever.
+     </p>
+    <img src="https://mintortho.com/wp-content/uploads/2024/03/Mouthguards-1024x681.jpg"
+    className='w-[550px] h-[400px] xl:w-full xl:h-full xl:px-5 py-5'
+    alt="" />
+    </div>
+
+    <div className='p-5'>
+    <ul>
+        <li className='text-mintGreen text-3xl font-bold'>
+            <a href="#Mouthguards">Mouthguards</a>
+        </li>
+    </ul>
+     <p className='pt-4 lg:text-2xl' id='Mouthguards'>Whether you spent months or years wearing braces or managed to avoid the orthodontics chair with perfectly aligned dental genetics, your teeth still need protecting. And we’re not just talking about using your retainers, 
+        daily flossing, and making your 6-month dental check-ups a major priority.
+     </p>
+    <img src="https://mintortho.com/wp-content/uploads/2024/03/Sleep-Apnea-1024x683.jpg"
+    className='w-[550px] h-[400px] xl:w-full xl:h-full xl:px-5 py-5'
+    alt="" />
+    </div>
+
+  </div>
+    </>
+}
